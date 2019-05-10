9d49c87744c5b8e204c0c9453c067ed558321c21
Griger204 <47332474+Griger204@users.noreply.github.com>
2019-05-10 07:36:47 -0400

    Create paragraphs.js

---
+++ paragraphs.js
@@ -0,0 +1,19 @@
+function Instructions(){
+   text("* Use the '-->', '<--' and 'Space' Keys to move and jump!", 50, 100);
+  text("* Avoid Obstacles and Holes", 50, 115);
+  text("* Start with 3 lives but you get +1 every Level Passed! (Up to 10)", 50, 130);
+  text("* When Level Completed, you must answer 3 questions to continue! (6 tries ONLY)", 50, 145);
+  text("* Collect coins on your way!", 50, 160);
+  text("* GAME OVER = got questions wrong 6 times OR ran out of lives OR finished the game", 50, 175);
+  text("MAIN OBJECTIVE: Get your character back home while avoiding obstacles and answering questions!", 40, 200);
+}
+
+function AboutUs(){
+  text("We are Gal and Jordan. We are in grade 9 and currently taking the computer science class ", 50, 180);
+  text(" in AY Jackson. For our final project, we created a fun and interactive side-scroller game", 48, 196);
+  text("called:", 50, 212);
+  textSize(10);
+  text("ENJOY!", 330, 253);
+  textSize(20);
+  text("Royal Run!", 225, 245);
+}
