f633227ecdcd73b49e111225cd6647c0faa30914
Griger204 <47332474+Griger204@users.noreply.github.com>
2019-06-02 18:52:15 -0400

    Update block.js

--- block.js
+++ block.js
@@ -1,18 +1,13 @@
- class block {
-  constructor(){
-    this.x = 500;
-    this.y = 270;
+class block {
+  constructor(x,y){
+    this.x = x;
+    this.y = y;
     this.length = 20;
-    this.height = 60;
+    this.height = 20;
   }
   display(){
     fill(0, 0, 0);
     rect(this.x, this.y, this.length, this.height);
-  }
-  // contact(){
-  //   if(person.pos.x === obstacles.x){
-  //     drawScene6();
-  //     //life--;
-  //   }
-  // }
-}
+   }
+  
+  
